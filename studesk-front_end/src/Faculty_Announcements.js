9c745ebee806f005100babff34d6015bfba0e85a
yuvraj024 <oberoiyuvraj024@gmail.com>
2023-10-24 09:34:42 +0530

    add

--- studesk-front_end/src/Faculty_Announcements.js
+++ studesk-front_end/src/Faculty_Announcements.js
@@ -1,40 +1,46 @@
 import React, { useState } from 'react';
 import './FacultyAnnouncement.css';
+
 const Faculty_Announcements = () => {
-  const [input, setInput] = useState("");
+  const [input, setInput] = useState('');
   const [items, setItems] = useState([]);
+  
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    if(input.trim()===""){
+    if (input.trim() === '') {
       return;
     }
     setItems([...items, input]);
-    setInput("");
+    setInput('');
   };
 
-  return (
-    <div className='announcement-component'>
-      <h2>Add Announcement</h2>
-      <ul>
-        {items.map((item) => (
-          <li key={item}>{item}</li>
-        ))}
-      </ul>
-      <input
-        type="text"
-        placeholder="Enter an item..."
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        required
-      />
-      <button type="submit" onClick={handleSubmit}>
-        Submit
-      </button>
 
+  return (
+    <div className="announcement-component">
+      <h2>Add Announcement</h2>
+      <div className="announcement-list">
+        <ul>
+          {items.map((item, index) => (
+            <li key={index}>{item}</li>
+          ))}
+        </ul>
+      </div>
+      <div className="announcement-input-container">
+        <input
+          className="announcement-input"
+          type="text"
+          placeholder="Enter an item..."
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          required
+        />
+        <button className="announcement-submit" type="submit" onClick={handleSubmit}>
+          Submit
+        </button>
+      </div>
     </div>
   );
 };
-
 
 export default Faculty_Announcements;
